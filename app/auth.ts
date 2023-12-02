475ec6f923475a9acabb5a87878abb23a2c79520
dylwong <85528459+dylwong@users.noreply.github.com>
2023-12-01 21:26:40 -0800

    Update auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -1,15 +1,18 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+// Import the necessary modules for custom OAuth provider
+import Providers from 'next-auth/providers';
 
-export const {
-  handlers: { GET, POST },
-  auth
-} = NextAuth({
+export const { handlers: { GET, POST }, auth } = NextAuth({
   providers: [
-    GitHub({
-      clientId: process.env.OAUTH_CLIENT_KEY as string,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET as string
-    })
+    Providers.OAuth2({
+      id: 'openshift', // Custom ID for the provider
+      name: 'OpenShift', // Name of the provider
+      clientId: process.env.OPENSHIFT_OAUTH_CLIENT_ID,
+      clientSecret: process.env.OPENSHIFT_OAUTH_CLIENT_SECRET,
+      authorizationUrl: 'https://<openshift-cluster-url>/oauth/authorize?response_type=code',
+      accessTokenUrl: 'https://<openshift-cluster-url>/oauth/token',
+      // Additional configuration might be required here depending on OpenShift's OAuth implementation
+    }),
   ],
   pages: {
     signIn: '/sign-in'
